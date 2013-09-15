fce73254bd328dfc757e1816f34a129c7390adc7
ganymedes01 <Andre@pc-215-232.wireless.stchris.uod.lan>
2013-09-15 02:06:44 +0100

    Version 1.3.1

--- common/ganymedes01/ganyssurface/lib/Reference.java
+++ common/ganymedes01/ganyssurface/lib/Reference.java
@@ -14,7 +14,7 @@ public class Reference {
 	public static final String DEPENDENCIES = "required-after:Forge@[9.10.1.849,)";
 	public static final String CHANNEL_NAME = "GanysSurface";
 	public static final String MASTER = "GanysMods";
-	public static final String VERSION_NUMBER = "1.3.0";
+	public static final String VERSION_NUMBER = "1.3.1";
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ARMOUR_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/models/armor/";
 	public static final String ENTITY_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/entities/";
