56b45bc5561fa6c7eb33932969f50021fa6b3512
ganymedes02 <Andre@andre.home>
2013-10-07 21:19:48 +0100

    Organising Utils

--- common/ganymedes01/ganyssurface/core/utils/Utils.java
+++ common/ganymedes01/ganyssurface/core/utils/Utils.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
@@ -81,67 +82,81 @@ public class Utils {
 	}
 
 	public static final boolean addEntitytoInventory(IInventory iinventory, EntityItem item) {
-		if (item.getEntityItem() == null || item.getEntityItem().stackSize <= 0)
-			return false;
-		ArrayList<Integer> slots = getStackSlots(iinventory, item.getEntityItem());
-
-		while (slots.size() > 0 && item.getEntityItem().stackSize > 0) {
-			for (Integer slot : slots)
-				while (iinventory.getStackInSlot(slot).stackSize < iinventory.getStackInSlot(slot).getMaxStackSize() && item.getEntityItem().stackSize > 0) {
-					iinventory.getStackInSlot(slot).stackSize++;
-					item.getEntityItem().stackSize--;
-				}
-			slots = getStackSlots(iinventory, item.getEntityItem());
-		}
-		if (item.getEntityItem().stackSize <= 0) {
+		boolean flag = addStackToInventory(iinventory, item.getEntityItem());
+		if (item.getEntityItem().stackSize <= 0)
 			item.setDead();
-			return true;
-		}
-
-		for (int i = 0; i < iinventory.getSizeInventory(); i++)
-			if (iinventory.getStackInSlot(i) == null) {
-				iinventory.setInventorySlotContents(i, item.getEntityItem());
-				item.setDead();
-				return true;
-			}
-		return false;
+		return flag;
 	}
 
-	public static final boolean addStacktoInventory(IInventory iinventory, ItemStack stack) {
+	public static final boolean addStackToInventory(IInventory iinventory, ItemStack stack) {
+		return addStackToInventory(iinventory, stack, -1);
+	}
+
+	public static final boolean addStackToInventory(IInventory iinventory, ItemStack stack, int side) {
 		if (stack == null || stack.stackSize <= 0)
 			return false;
-		ArrayList<Integer> slots = getStackSlots(iinventory, stack);
+		ArrayList<Integer> slots = getStackSlots(iinventory, stack, side);
 
-		while (slots.size() > 0 && stack.stackSize > 0) {
+		while (!slots.isEmpty() && stack.stackSize > 0) {
 			for (Integer slot : slots)
-				while (iinventory.getStackInSlot(slot).stackSize < iinventory.getStackInSlot(slot).getMaxStackSize() && stack.stackSize > 0) {
+				while (iinventory.getStackInSlot(slot).stackSize < iinventory.getStackInSlot(slot).getMaxStackSize() && iinventory.getStackInSlot(slot).stackSize < iinventory.getInventoryStackLimit() && stack.stackSize > 0) {
 					iinventory.getStackInSlot(slot).stackSize++;
 					stack.stackSize--;
 				}
-			slots = getStackSlots(iinventory, stack);
+			slots = getStackSlots(iinventory, stack, side);
 		}
 		if (stack.stackSize <= 0)
 			return true;
-
-		for (int i = 0; i < iinventory.getSizeInventory(); i++)
-			if (iinventory.getStackInSlot(i) == null) {
-				iinventory.setInventorySlotContents(i, stack.copy());
-				stack.stackSize = 0;
-				return true;
-			}
+		else if (iinventory instanceof ISidedInventory) {
+			for (int slot : ((ISidedInventory) iinventory).getAccessibleSlotsFromSide(side))
+				if (insertStackToNullSlot(slot, iinventory, stack, side))
+					return true;
+		} else
+			for (int i = 0; i < iinventory.getSizeInventory(); i++)
+				if (insertStackToNullSlot(i, iinventory, stack, side))
+					return true;
 		return false;
 	}
 
-	private static final ArrayList<Integer> getStackSlots(IInventory iinventory, ItemStack stack) {
+	private static final boolean insertStackToNullSlot(int i, IInventory iinventory, ItemStack stack, int side) {
+		if (iinventory.getStackInSlot(i) == null && iinventory.isItemValidForSlot(i, stack)) {
+			if (stack.stackSize > iinventory.getInventoryStackLimit()) {
+				iinventory.setInventorySlotContents(i, new ItemStack(stack.itemID, iinventory.getInventoryStackLimit(), stack.getItemDamage()));
+				stack.stackSize -= iinventory.getInventoryStackLimit();
+			} else {
+				iinventory.setInventorySlotContents(i, stack.copy());
+				stack.stackSize = 0;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private static final ArrayList<Integer> getStackSlots(IInventory iinventory, ItemStack stack, int side) {
 		ArrayList<Integer> slots = new ArrayList<Integer>();
-		if (stack.stackSize > 0)
+		if (stack.stackSize <= 0)
+			return slots;
+
+		if (iinventory instanceof ISidedInventory && side > -1) {
+			for (int slot : ((ISidedInventory) iinventory).getAccessibleSlotsFromSide(side))
+				if (checkForStackSlots(slot, iinventory, stack))
+					slots.add(slot);
+		} else
 			for (int i = 0; i < iinventory.getSizeInventory(); i++)
-				if (iinventory.getStackInSlot(i) != null && stack != null)
-					if (iinventory.getStackInSlot(i).getItem() == stack.getItem())
-						if (iinventory.getStackInSlot(i).isItemEqual(stack))
-							if (iinventory.getStackInSlot(i).stackSize < iinventory.getStackInSlot(i).getMaxStackSize())
-								slots.add(i);
+				if (checkForStackSlots(i, iinventory, stack))
+					slots.add(i);
 		return slots;
+	}
+
+	private static final boolean checkForStackSlots(int slot, IInventory iinventory, ItemStack stack) {
+		if (iinventory.getStackInSlot(slot) != null && stack != null)
+			if (iinventory.isItemValidForSlot(slot, stack))
+				if (iinventory.getStackInSlot(slot).getItem() == stack.getItem())
+					if (iinventory.getStackInSlot(slot).isItemEqual(stack))
+						if (iinventory.getStackInSlot(slot).stackSize < iinventory.getStackInSlot(slot).getMaxStackSize())
+							if (iinventory.getStackInSlot(slot).stackSize < iinventory.getInventoryStackLimit())
+								return true;
+		return false;
 	}
 
 	public static final String CHAT_COLOUR_BLACK = "\u00a70";
