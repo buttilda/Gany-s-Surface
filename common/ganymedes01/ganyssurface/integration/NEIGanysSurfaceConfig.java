2107689537db2e6b240ac467c04963834ca2d2be
ganymedes02 <Andre@andre.home>
2013-11-09 16:26:00 +0000

    Added [?] button fuctionality for the Work Table

--- common/ganymedes01/ganyssurface/integration/NEIGanysSurfaceConfig.java
+++ common/ganymedes01/ganyssurface/integration/NEIGanysSurfaceConfig.java
@@ -1,10 +1,12 @@
 package ganymedes01.ganyssurface.integration;
 
 import ganymedes01.ganyssurface.blocks.ModBlocks;
+import ganymedes01.ganyssurface.client.gui.inventory.GuiWorkTable;
 import ganymedes01.ganyssurface.items.ModItems;
 import ganymedes01.ganyssurface.lib.Reference;
 import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
+import codechicken.nei.recipe.DefaultOverlayHandler;
 
 /**
  * Gany's Surface
@@ -17,6 +19,9 @@ public class NEIGanysSurfaceConfig implements IConfigureNEI {
 
 	@Override
 	public void loadConfig() {
+		API.registerGuiOverlay(GuiWorkTable.class, "crafting");
+		API.registerGuiOverlayHandler(GuiWorkTable.class, new DefaultOverlayHandler(), "crafting");
+
 		API.hideItem(ModBlocks.camelliaCrop.blockID);
 		API.hideItem(ModItems.mankyCupOfTea.itemID);
 	}
