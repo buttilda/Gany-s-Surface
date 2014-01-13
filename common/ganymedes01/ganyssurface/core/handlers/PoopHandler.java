89785af580a56ba493b5543182c9e83ce449eb7b
ganymedes01 <foka_12@hotmail.com>
2014-01-13 22:43:41 +0000

    This should fix pooping

--- common/ganymedes01/ganyssurface/core/handlers/PoopHandler.java
+++ common/ganymedes01/ganyssurface/core/handlers/PoopHandler.java
@@ -1,10 +1,13 @@
 package ganymedes01.ganyssurface.core.handlers;
 
 import ganymedes01.ganyssurface.blocks.ModBlocks;
+import ganymedes01.ganyssurface.core.utils.Utils;
+import ganymedes01.ganyssurface.items.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityBat;
 import net.minecraft.entity.passive.EntityTameable;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
@@ -34,12 +37,12 @@ public class PoopHandler {
 
 	private void replaceNearbyAirBlock(World world, int x, int y, int z, Block block, int meta) {
 		for (int i = -1; i < 2; i++)
-			for (int j = -1; j < 2; j++)
-				for (int k = -1; k < 2; k++)
-					if (world.isAirBlock(x + i, y + j, z + k) || Block.blocksList[world.getBlockId(x + i, y + j, z + k)].isBlockReplaceable(world, x + i, y + j, z + k)) {
-						world.setBlock(x + i, y + j, z + k, block.blockID, meta, 3);
-						ModBlocks.poop.onNeighborBlockChange(world, x + i, y + j, z + k, block.blockID);
-						return;
-					}
+			for (int k = -1; k < 2; k++)
+				if (world.isAirBlock(x + i, y, z + k) || Block.blocksList[world.getBlockId(x + i, y, z + k)].isBlockReplaceable(world, x + i, y, z + k)) {
+					world.setBlock(x + i, y, z + k, block.blockID, meta, 3);
+					ModBlocks.poop.onNeighborBlockChange(world, x + i, y, z + k, block.blockID);
+					return;
+				}
+		Utils.dropStack(world, x, y, z, new ItemStack(ModItems.poop, 1, meta));
 	}
 }
