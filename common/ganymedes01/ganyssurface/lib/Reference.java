e2814514c0c00b46ead149b85f398109cec35f1e
ganymedes01 <foka_12@hotmail.com>
2014-03-14 14:45:38 +0000

    Version 1.6

--- common/ganymedes01/ganyssurface/lib/Reference.java
+++ common/ganymedes01/ganyssurface/lib/Reference.java
@@ -14,14 +14,13 @@ public class Reference {
 	public static final String DEPENDENCIES = "required-after:Forge@[9.10.1.919,)";
 	public static final String CHANNEL_NAME = "GanysSurface";
 	public static final String MASTER = "GanysMods";
-	public static final String VERSION_NUMBER = "1.5.5";
-	public static final int RAW_VERSION_NUMBER = 155;
+	public static final String VERSION_NUMBER = "1.6.0";
+	public static final int RAW_VERSION_NUMBER = 160;
 	public static String LATEST_VERSION;
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ARMOUR_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/models/armor/";
 	public static final String ENTITY_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/entities/";
 	public static final String GUI_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/gui/container/";
-	public static final String FILLER_PATTER_TEXTURE_PATH = MOD_ID + ":" + "fillerPatterns/";
 
 	public static final String SERVER_PROXY_CLASS = "ganymedes01.ganyssurface.core.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "ganymedes01.ganyssurface.core.proxy.ClientProxy";
