f55b597c71a3791f86c2bcbb35fe3eada145ae1d
ganymedes01 <foka_12@hotmail.com>
2014-04-19 03:42:01 -0300

    Added wings to pocket bat

--- common/ganymedes01/ganyssurface/client/model/ModelPocketBat.java
+++ common/ganymedes01/ganyssurface/client/model/ModelPocketBat.java
@@ -16,6 +16,8 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ModelPocketBat extends ModelBase {
 
 	private final ModelRenderer batHead, batBody;
+	private final ModelRenderer batRightWing, batLeftWing;
+	private final ModelRenderer batOuterRightWing, batOuterLeftWing;
 
 	public ModelPocketBat() {
 		textureWidth = 64;
@@ -32,6 +34,29 @@ public class ModelPocketBat extends ModelBase {
 		batBody = new ModelRenderer(this, 0, 16);
 		batBody.addBox(-3.0F, 4.0F, -3.0F, 6, 12, 6);
 		batBody.setTextureOffset(0, 34).addBox(-5.0F, 16.0F, 0.0F, 10, 6, 1);
+		batRightWing = new ModelRenderer(this, 42, 0);
+		batRightWing.addBox(-12.0F, 1.0F, 1.5F, 10, 16, 1);
+		batOuterRightWing = new ModelRenderer(this, 24, 16);
+		batOuterRightWing.setRotationPoint(-12.0F, 1.0F, 1.5F);
+		batOuterRightWing.addBox(-8.0F, 1.0F, 0.0F, 8, 12, 1);
+		batLeftWing = new ModelRenderer(this, 42, 0);
+		batLeftWing.mirror = true;
+		batLeftWing.addBox(2.0F, 1.0F, 1.5F, 10, 16, 1);
+		batOuterLeftWing = new ModelRenderer(this, 24, 16);
+		batOuterLeftWing.mirror = true;
+		batOuterLeftWing.setRotationPoint(12.0F, 1.0F, 1.5F);
+		batOuterLeftWing.addBox(0.0F, 1.0F, 0.0F, 8, 12, 1);
+		batBody.addChild(batRightWing);
+		batBody.addChild(batLeftWing);
+		batRightWing.addChild(batOuterRightWing);
+		batLeftWing.addChild(batOuterLeftWing);
+
+		batRightWing.setRotationPoint(0.0F, 0.0F, 0.0F);
+		batLeftWing.setRotationPoint(0.0F, 0.0F, 0.0F);
+		batBody.rotateAngleX = 0.0F;
+		batBody.rotateAngleY = 0.0F;
+		batRightWing.rotateAngleY = (float) Math.PI;
+		batLeftWing.rotateAngleY = -batRightWing.rotateAngleY;
 	}
 
 	public void renderAll() {
