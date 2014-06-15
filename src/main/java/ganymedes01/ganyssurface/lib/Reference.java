cb3b3db3aff3e12c2f366ff79926f4a42e039509
ganymedes01 <foka_12@hotmail.com>
2014-06-15 03:51:07 +0100

    Prep for tomorrow's release

--- src/main/java/ganymedes01/ganyssurface/lib/Reference.java
+++ src/main/java/ganymedes01/ganyssurface/lib/Reference.java
@@ -2,20 +2,20 @@ package ganymedes01.ganyssurface.lib;
 
 /**
  * Gany's Surface
- * 
+ *
  * @author ganymedes01
- * 
+ *
  */
 
 public class Reference {
 
 	public static final String MOD_ID = "ganyssurface";
 	public static final String MOD_NAME = "Gany's Surface";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.12.1.1111,)";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.12.1.1124,)";
 	public static final String CHANNEL = "GanysSurface";
 	public static final String MASTER = "GanysMods";
-	public static final String VERSION_NUMBER = "1.7.0";
-	public static final int RAW_VERSION_NUMBER = 170;
+	public static final String VERSION_NUMBER = "1.7.1";
+	public static final int RAW_VERSION_NUMBER = 171;
 	public static String LATEST_VERSION;
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ARMOUR_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/models/armor/";
