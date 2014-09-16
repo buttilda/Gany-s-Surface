b7f686c8c1730867c28293996c4f4d839dc639ac
ganymedes01 <foka_12@hotmail.com>
2014-09-17 00:14:28 +0100

    Added clay block to clay balls recipe

--- src/main/java/ganymedes01/ganyssurface/recipes/ModRecipes.java
+++ src/main/java/ganymedes01/ganyssurface/recipes/ModRecipes.java
@@ -158,7 +158,8 @@ public class ModRecipes {
 
 		// Vanilla
 		if (GanysSurface.enableExtraVanillaRecipes) {
-			GameRegistry.addRecipe(new ItemStack(Items.clay_ball, 8), "xxx", "yzy", "xxx", 'x', Blocks.gravel, 'y', Blocks.dirt, 'z', Items.water_bucket);
+			GameRegistry.addRecipe(new ItemStack(Items.clay_ball, 4), "xxx", "yzy", "xxx", 'x', Blocks.gravel, 'y', Blocks.dirt, 'z', Items.water_bucket);
+			GameRegistry.addShapelessRecipe(new ItemStack(Items.clay_ball, 4), Blocks.clay);
 			GameRegistry.addRecipe(new ItemStack(Items.name_tag), " y ", "x  ", 'x', Items.paper, 'y', Items.string);
 			GameRegistry.addShapelessRecipe(new ItemStack(Items.fermented_spider_eye), Items.spider_eye, Items.sugar, Blocks.red_mushroom);
 		}
