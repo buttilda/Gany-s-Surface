36dd204e81c593e030aeb87345ff92382c724764
ganymedes01 <foka_12@hotmail.com>
2015-04-25 11:23:13 -0300

    Fix Coarse Dirt not allowing things to be planted on it.

--- src/main/java/ganymedes01/ganyssurface/blocks/CoarseDirt.java
+++ src/main/java/ganymedes01/ganyssurface/blocks/CoarseDirt.java
@@ -5,6 +5,10 @@ import ganymedes01.ganyssurface.core.utils.Utils;
 import ganymedes01.ganyssurface.lib.Strings;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.util.ForgeDirection;
 
 /**
  * Gany's Surface
@@ -22,5 +26,10 @@ public class CoarseDirt extends Block {
 		setBlockTextureName(Strings.COARSE_DIRT);
 		setBlockName(Utils.getUnlocalisedName(Strings.COARSE_DIRT));
 		setCreativeTab(GanysSurface.enableCoarseDirt ? GanysSurface.surfaceTab : null);
+	}
+
+	@Override
+	public boolean canSustainPlant(IBlockAccess world, int x, int y, int z, ForgeDirection direction, IPlantable plant) {
+		return Blocks.dirt.canSustainPlant(world, x, y, z, direction, plant);
 	}
 }
