5d4f757006d5c8d510c18ca955020235ed778918
ganymedes01 <foka_12@hotmail.com>
2015-07-10 01:09:53 -0300

    Implement shift+clicking on market slots

--- src/main/java/ganymedes01/ganyssurface/inventory/ContainerMarket.java
+++ src/main/java/ganymedes01/ganyssurface/inventory/ContainerMarket.java
@@ -17,8 +17,11 @@ import net.minecraft.item.ItemStack;
 
 public class ContainerMarket extends GanysContainer {
 
+	private final TileEntityMarket market;
+
 	public ContainerMarket(InventoryPlayer player, TileEntityMarket market) {
 		super(market);
+		this.market = market;
 
 		// Display product
 		addSlotToContainer(new MarketSlot(market, 0, 12, 29));
@@ -40,7 +43,41 @@ public class ContainerMarket extends GanysContainer {
 
 	@Override
 	public ItemStack transferStackInSlot(EntityPlayer player, int slotIndex) {
-		return null;
+		ItemStack itemstack = null;
+		Slot slot = (Slot) inventorySlots.get(slotIndex);
+
+		if (slot != null && slot.getHasStack()) {
+			ItemStack stack = slot.getStack();
+			itemstack = stack.copy();
+
+			if (slotIndex > 6) {
+				if (slotIndex > 13) {
+					boolean merged = false;
+					for (int i = 7; i < 13; i++)
+						if (market.isItemValidForSlot(i, stack))
+							if (!mergeItemStack(stack, i, i + 1, false))
+								return null;
+							else
+								merged = true;
+					if (!merged)
+						return null;
+				} else if (!mergeItemStack(stack, 14, 50, true))
+					return null;
+			} else
+				return null;
+
+			if (stack.stackSize == 0)
+				slot.putStack((ItemStack) null);
+			else
+				slot.onSlotChanged();
+
+			if (stack.stackSize == itemstack.stackSize)
+				return null;
+
+			slot.onPickupFromSlot(player, stack);
+		}
+
+		return itemstack;
 	}
 
 	@Override
