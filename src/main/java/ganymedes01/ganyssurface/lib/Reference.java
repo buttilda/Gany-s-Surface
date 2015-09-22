154d2db8704147cf0a000ad543cf3de10ede0d18
ganymedes01 <foka_12@hotmail.com>
2015-09-22 03:50:21 +0100

    Bump version

--- src/main/java/ganymedes01/ganyssurface/lib/Reference.java
+++ src/main/java/ganymedes01/ganyssurface/lib/Reference.java
@@ -14,8 +14,8 @@ public class Reference {
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,);";
 	public static final String CHANNEL = "GanysSurface";
 	public static final String MASTER = "GanysMods";
-	public static final String VERSION_NUMBER = "1.12.5";
-	public static final int RAW_VERSION_NUMBER = 1125;
+	public static final String VERSION_NUMBER = "1.12.6";
+	public static final int RAW_VERSION_NUMBER = 1126;
 	public static String LATEST_VERSION;
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ARMOUR_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/models/armor/";
